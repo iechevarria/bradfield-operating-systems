43e10afc07b36f0621a2591b20eeed915522def9
Ivan Echevarria <ivan@ladderlife.com>
2021-05-22 00:21:20 -0700

    finish mini miner lab

--- lab3-miner/miner.c
+++ lab3-miner/miner.c
@@ -3,27 +3,25 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 #include "sha-256.h"
 
 #define DIFFICULTY 2 // number of bytes of leading 0's
-#define WORKER_COUNT 1 // number of worker threads
-#define BUF_SIZE 256 // number of entries in ring buffer
+#define WORKER_COUNT 4 // number of worker threads
+#define BUF_SIZE 16 // number of entries in ring buffer
+
+uint32_t nonce, total;
 
 //
 // thread stuff
 //
 
 pthread_t thread_ids[WORKER_COUNT];
-int successes[WORKER_COUNT];
+int successes[WORKER_COUNT] = {0};
 
-// how do you do this the right way?
-// this is so so so dumb
-int stupid_way_to_pass_int[4] = {1, 2, 3, 4};
-
-uint32_t nonce;
+// how do you do this the right way? this is so so so dumb
+int stupid_way_to_pass_int[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 //
 // ring buffer stuff
@@ -31,6 +29,7 @@ uint32_t nonce;
 
 struct mine_result {
   uint32_t value;
+  uint32_t target;
   int worker;
 };
 
@@ -39,7 +38,6 @@ int wp = 0;
 int rp = 0;
 sem_t *readable;
 sem_t *writable;
-
 pthread_mutex_t lock;
 
 
@@ -48,12 +46,14 @@ void _print_hash(uint8_t hash[32]) {
     printf("%02x", hash[i]);
 }
 
+
 int successful(uint8_t hash[32]) {
   for (int i = 0; i < DIFFICULTY; i++)
     if (hash[i] != 0)
       return 0;
   return 1;
 }
+
 
 void *thread_func(void *id) {
   int worker = *(int *) id;
@@ -66,20 +66,10 @@ void *thread_func(void *id) {
     do {
       x = rand();
       sum = nonce + x;
-      memset(hash, 0x00, sizeof hash);
-      calc_sha_256(hash, (const void *)(&sum), 32);
+      calc_sha_256(hash, (const void *)(&sum), 4);
     } while (!successful(hash));
 
-  
-    struct mine_result to_write = {.value=x, .worker=worker};
-    _print_hash(hash);
-    printf("   sum: %u\n", sum);
-
-    memset(hash, 0x00, sizeof hash);
-    calc_sha_256(hash, (const void *)(&sum), 32);
-    _print_hash(hash);
-    printf("   sum: %u\n", sum);
-
+    struct mine_result to_write = {.value=x, .target=nonce, .worker=worker};
     sem_wait(writable);
 
     pthread_mutex_lock(&lock);
@@ -93,11 +83,8 @@ void *thread_func(void *id) {
   return NULL;
 }
 
-int main() {
-  // TODO instead of just running this on one nonce, generate
-  // a sequence of random nonces and have miners hash the nonce
-  // separately in threads
 
+int main() {
   sem_unlink("/tmp/bsem1");
   sem_unlink("/tmp/bsem2");
   readable = sem_open("/tmp/bsem1", O_CREAT, 0644, 0);
@@ -109,35 +96,45 @@ int main() {
     pthread_create(&thread_ids[i], NULL, thread_func, &stupid_way_to_pass_int[i]);
   }
 
-  // add values
   srand(time(NULL));  
   nonce = rand();
 
-  // do validation
   uint8_t hash[32];
   struct mine_result cur;
-  uint32_t sum;
+  uint32_t sum, prev;
 
   while (1) {
     sem_wait(readable);
 
+    // do validation
     cur = buffer[rp];
     sum = nonce + cur.value;
-    memset(hash, 0x00, sizeof hash);
-    calc_sha_256(hash, (const void *)(&sum), 32);
-
-    _print_hash(hash);
-    printf("   sum: %u\n", sum);
-    printf("successful? %d\n", successful(hash));
+    calc_sha_256(hash, (const void *)(&sum), 4);
 
     if (successful(hash)) {
-      printf("Worker %d successfully mined %u\n", cur.worker, cur.value);
       successes[cur.worker] += 1;
+      total += 1;
+
+      if (total % 50 == 0) {
+        printf(
+          "leaderboard (total %d):\n0: %d\n1: %d\n2: %d\n3: %d\n",
+          total,
+          successes[0],
+          successes[1],
+          successes[2],
+          successes[3]
+        );
+      }
+
+      prev = nonce;
       nonce = rand();
+    } else {
+      // log unsuccessful attempts, see why failed
+      printf("FAIL target: %u / nonce: %u / prev: %u\n", cur.target, nonce, prev);
     }
 
-    printf("rp is %d\n", rp);
     rp = (rp + 1) % BUF_SIZE;
+
     sem_post(writable);
   }
 }
